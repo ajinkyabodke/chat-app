9d027fa70c5cfa6eeccaa026475ce5f762e634cb
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2023-08-17 15:06:04 +0530

    Update UI to join and exit room

--- public/script.js
+++ public/script.js
@@ -1,10 +1,35 @@
 $(function() {
     var socket = io();
+    var username = "";
+
+    $('#join-button').click(function() {
+        username = $('#username').val();
+        if (username.trim() === "") {
+            alert("Please enter a valid username.");
+            return;
+        }
+
+        $('#username-form').hide();
+        $('#logged-in-username').text(username);
+        $('#chat').show();
+
+        socket.emit('chat message', `${username} has joined the chat.`);
+    });
+
+    $('#leave-button').click(function() {
+        socket.emit('chat message', `${username} has left the chat.`);
+        $('#chat').hide();
+        $('#username-form').show();
+        $('#messages').empty();
+    });
+
     $('form').submit(function() {
-        socket.emit('chat message', $('#input').val());
+        var message = $('#input').val();
+        socket.emit('chat message', `${username}: ${message}`);
         $('#input').val('');
         return false;
     });
+
     socket.on('chat message', function(msg) {
         $('#messages').append($('<li>').text(msg));
     });
