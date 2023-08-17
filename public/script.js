c01dd70b5964d267b9d505273b67b63e5601fb2a
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2023-08-17 15:30:34 +0530

    Add User Color

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 $(function() {
     var socket = io();
     var username = "";
+    var userColors = {};
 
     $('#join-button').click(function() {
         username = $('#username').val();
@@ -9,15 +10,19 @@ $(function() {
             return;
         }
 
+        if (!userColors[username]) {
+            userColors[username] = getRandomColor();
+        }
+
         $('#username-form').hide();
         $('#logged-in-username').text(username);
         $('#chat').show();
 
-        socket.emit('chat message', `${username} has joined the chat.`);
+        socket.emit('chat message', { message: `${username} has joined the chat.`, color: userColors[username] });
     });
 
     $('#leave-button').click(function() {
-        socket.emit('chat message', `${username} has left the chat.`);
+        socket.emit('chat message', { message: `${username} has left the chat.`, color: userColors[username] });
         $('#chat').hide();
         $('#username-form').show();
         $('#messages').empty();
@@ -25,12 +30,21 @@ $(function() {
 
     $('form').submit(function() {
         var message = $('#input').val();
-        socket.emit('chat message', `${username}: ${message}`);
+        socket.emit('chat message', { message: `${username}: ${message}`, color: userColors[username] });
         $('#input').val('');
         return false;
     });
 
-    socket.on('chat message', function(msg) {
-        $('#messages').append($('<li>').text(msg));
+    socket.on('chat message', function(data) {
+        $('#messages').append($('<li>').html(`<span style="color: ${data.color};">${data.message}</span>`));
     });
 });
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
